1df8bc79b0f0f0f6542a996e5b9d883556876bcf
Gurjas Singh <gurjassingh92@gmail.com>
2021-02-08 18:31:55 +0530

    password changed to 12345

--- home.py
+++ home.py
@@ -34,7 +34,7 @@ def profile():
             'password':password
         }
 
-        if ((name == 'rajat')&(password == '12345678')):
+        if ((name == 'rajat')&(password == '12345')):
             return render_template('home_page.html')
 
 @app.route("/data_display", methods=['POST'])
